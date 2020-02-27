134788decf02fc05122b26a513902df4276f62a9
ion_chris <chris@i-on.net>
2020-02-27 14:23:28 +0900

    삽입 정렬 학습 - 걸리는 시간 정리함

--- study/20200227_INSERT_SORT/INSERT_SORT.py
+++ study/20200227_INSERT_SORT/INSERT_SORT.py
@@ -1,4 +1,6 @@
 import random
+import time
+start = time.time()
 data_list = random.sample(range(100), 50)
 print(data_list)
 '''
@@ -20,3 +22,4 @@ def insert_sort(data):
     return data
 
 print(insert_sort(data_list))
+print("time :", time.time() - start)
